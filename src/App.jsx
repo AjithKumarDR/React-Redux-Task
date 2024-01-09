5a4fa2a6a307162739d7956daeb70d7eeafb9f5f
AjithKumarDR <ajithkumardr1995@gmail.com>
2024-01-09 06:46:50 +0530

    Redux File Upload

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,27 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Button from 'react-bootstrap/Button';
+// import CartsContextComponent from "./utils/CartsContextComponent"
+import Cartss from "./components/Cartss"
+
+
+import {BrowserRouter,Routes,Route,Navigate} from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0)
 
-  return (
-    <>
-     <h1>Please Give Me time to Complete</h1>
+  return <>     
+  <BrowserRouter>
+       <Routes>
+       {/* <Route path="/Carts" element={<CartsContextComponent><Cartss /></CartsContextComponent>}/> */}
+       <Route path="/Carts" element={<Cartss />}/>
+       <Route path="*" element={<Navigate to='/Carts'/>}/>
+       </Routes>
+       </BrowserRouter>
+      
     </>
-  )
+  
 }
 
 export default App
